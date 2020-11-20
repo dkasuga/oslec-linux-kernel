e906687d4f1b42895b4b5577fed2c915a6592cb7
dkasuga <benjamin.at.mac@gmail.com>
2020-11-20 22:21:15 +0900

    add change_scheduler

---
+++ kadai_sche/kadai2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sched.h>
+
+
+void 
+check_scheduler_policy(int pid){
+    int policy;
+    policy = sched_getscheduler(pid);
+
+    switch(policy){
+        case SCHED_OTHER: printf("SCHED_OTHER\n"); break;
+        case SCHED_RR:   printf("SCHED_RR\n"); break;
+        case SCHED_FIFO:  printf("SCHED_FIFO\n"); break;
+        default:   printf("Unknown...\n");
+    }
+}
+
+int
+main(){
+    struct sched_param param;
+    int pid = 0;
+    int ret;
+    int policy;
+
+    printf("Original Scheduler Policy:");
+    check_scheduler_policy(pid);
+    /*
+    param.sched_priority=99;
+    ret = sched_setscheduler(pid_num, SCHED_FIFO, &param);
+
+    if(ret == -1){
+        perror("sched_setscheduler");
+        return -1;
+    }
+    */
+
+    return 0;
+}
